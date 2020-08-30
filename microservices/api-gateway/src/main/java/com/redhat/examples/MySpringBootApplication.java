5453d334fe97df920f8b135e17d8615ed1e07f5c
mites1997 <arnabfeb15@gmail.com>
2020-08-30 11:04:56 +0530

    add demo microservices with docker and kubernetes config

---
+++ microservices/api-gateway/src/main/java/com/redhat/examples/MySpringBootApplication.java
@@ -0,0 +1,19 @@
+package com.redhat.examples;
+
+import org.apache.camel.opentracing.starter.CamelOpenTracing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.*;
+
+@SpringBootApplication
+@CamelOpenTracing
+public class MySpringBootApplication {
+
+    /**
+     * A main method to start this application.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootApplication.class, args);
+    }
+
+}
